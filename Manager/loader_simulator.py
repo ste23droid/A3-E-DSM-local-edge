fa2f5d096a79bf832f0958268c37537dcf5b44d0
Ste <stefano.brandoli23@gmail.com>
2019-03-11 10:08:47 +0100

    Load fixed

--- Manager/loader_simulator.py
+++ Manager/loader_simulator.py
@@ -70,7 +70,7 @@ class LoaderSimulator:
                           verify=False,
                           headers=config.APPLICATION_JSON_HEADER)
             delta_seconds = time.time() - start_request
-            print("Time to make load request sec {}".format(delta_seconds))
+            #print("Time to make load request sec {}".format(delta_seconds))
             #print(post_request.json())
 
 
@@ -97,16 +97,16 @@ class LoaderSimulator:
             # HIGH 0.7 sec
             # VERY HIGH 0.4
 
-            if deltaSec < 480:
-                time.sleep(2)
-            elif deltaSec < 480*2:
+            if deltaSec < 300:
                 time.sleep(1)
-            elif deltaSec < 480*3:
-                time.sleep(1.5)
-            elif deltaSec < 480*4:
-                time.sleep(0.7)
+            elif deltaSec < 300*2:
+                time.sleep(1)
+            elif deltaSec < 300*3:
+                time.sleep(1)
+            elif deltaSec < 300*4:
+                time.sleep(1)
             else:
-                time.sleep(3.5)
+                time.sleep(1)
 
             self.iterations = self.iterations + 1
 
